1465551432994b768922f16c476803b1b17cbfd4
tkohad <tkohad@gmail.com>
2023-12-02 08:26:55 +0530

    Commit

--- src/main/java/com/example/demo/Function/FunctionTest.java
+++ src/main/java/com/example/demo/Function/FunctionTest.java
@@ -1,4 +1,21 @@
 package com.example.demo.Function;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 public class FunctionTest {
+
+
+    public void test(){
+
+        List<String> list1=new ArrayList<>();
+        List<String> list2=new LinkedList<>();
+        List<String> list3=new Stack<>();
+
+        list1.contains("tets test teststst");
+
+
+    }
 }
